3dfddb45291d5289d69162b04967f78163d501fd
simondawall <simondanielwall@gmail.com>
2022-06-30 11:04:37 +0200

    Basic Config

---
+++ init.lua
@@ -0,0 +1,4 @@
+require "options"       -- General Options for standard neovim
+require "keymaps"       -- Basic Keymaps (more are inside of whichkey)
+require "plugins"       -- Plugin manager
+require "colorscheme"   -- Colorscheme configuration
